1c66af27d8d88ad5f29f9a5f17fba120334d893e
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-15 16:38:19 -0500

    tests

--- lab-beeksma/test/dataFromFile.test.js
+++ lab-beeksma/test/dataFromFile.test.js
@@ -4,15 +4,22 @@ const fileTest = require('../lib/dataFromFile');
 describe('dataFromFile', function() {
   describe('#logData()', function() {
     it ('should call callback with err if any file in the argument array supplied is not found and then retun undefined', function(done) {
-      fileTest.logData(['some','files','go','here'], function (err, data) {
-          //check for error here, use some different positions in the array
+      fileTest.logData(['one.txt','donkey.txt','three.txt'], function (err, res) {
+        assert.notStrictEqual(err, null);
+        assert.strictEqual(res, undefined);
         done();
       });
-      it ('should return an array with the first 8 characters of each file in the argument array in order', function(done) {
-        fileTest.logData(['some','files','go','here'], function (err, data) {
-            //check that the result array comes back with the correct sterf
-          done();
-        });
+    });
+    it ('should return an array with the first 8 characters of each file in the argument array in correct order', function(done) {
+      fileTest.logData(['one.txt','two.txt','three.txt'], function (err, res) {
+        assert.ifError(err);
+        assert.equal(res, ['48 61 72 61 6c 64 20 53','54 68 65 20 75 6e 61 6e','49 27 6d 20 70 61 63 6b']);
+        done();
+      });
+      fileTest.logData(['three.txt','two.txt','one.txt'], function (err, res) {
+        assert.ifError(err);
+        assert.equal(res, ['49 27 6d 20 70 61 63 6b','54 68 65 20 75 6e 61 6e','48 61 72 61 6c 64 20 53']);
+        done();
       });
     });
   });
